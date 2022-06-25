d8a3d168ce11ea451aba7fda03d29cffdc58fcfb
Brenok25 <87788919+Brenok25@users.noreply.github.com>
2022-06-25 11:40:12 -0300

    Criação banco tkhtmlview/place/grid

--- index.py
+++ index.py
@@ -1,23 +1,30 @@
 from tkinter import *
+from turtle import width
 from tkhtmlview import HTMLLabel
 root = Tk()
-root.geometry("500x600")
+root.geometry("903x600")
 root.title("Teste HTML")
+root.config(bg='#8a37cc')
+
+# creditos
+# <a href='https://br.freepik.com/fotos-vetores-gratis/fachada-de-vidro'>Fachada de vidro foto criado por diana.grytsku - br.freepik.com</a>
 
 
-my_label = HTMLLabel(root, html="\
-    <h1>HelloWorld</h1>\
-    <br>\
-    <p> oi </p>\
-    <p style='color: green;'>oi</p>").grid()
+# Title = HTMLLabel(root, html="<h1>Banco Farefaith</h1>").grid(row=0, column=0)
 
+fr1 = LabelFrame(root).grid()
 
+Title = Label(fr1, text="Banco qweojbk", font=("Mongolian Baiti", "25"),bg='#8a37cc', fg="#f5f5f5").grid(row=0, column=0, sticky=W, pady=15)
 
+cadastre = Button(fr1, text=" Cadastre-se ", bg="#eb8334", fg="#fff",width=15, height=1).place(bordermode=OUTSIDE, x=540, y=26)
+logar = Button(fr1, text=" Faça Login ", bg="#eb8334", fg="#fff",width=15, height=1).place(bordermode=OUTSIDE, x=660, y=26)
+about = Button(fr1, text=" Sobre nós ", bg="#eb8334", fg="#fff",width=15, height=1).place(bordermode=OUTSIDE, x=780, y=26)
 
+photo = HTMLLabel(fr1, html="<img src='chance.png'>", width=112,background='#8a37cc').grid(row=1, column=0, columnspan=4)
 
+about1 = HTMLLabel(root, html="<h2>Quem Somos?</h2>", background='#8a37cc',foreground='#f5f5f5').place(bordermode=OUTSIDE, x=320, y=360)
 
-
-
+about2 = HTMLLabel(root, html="<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>",background='#8a37cc',foreground='#f5f5f5').place(bordermode=OUTSIDE, x=250, y=400)
 
 
 root.mainloop()
